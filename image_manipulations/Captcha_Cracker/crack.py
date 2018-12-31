7fd7ec4ba6deee8ab64a36bdbce724204555abd7
WeiningLi <bill.weining.li@gmail.com>
2018-12-30 21:36:34 -0500

    [Captcha_Cracker] add part of the Chaptha cracker function

--- image_manipulations/Captcha_Cracker/crack.py
+++ image_manipulations/Captcha_Cracker/crack.py
@@ -1,4 +1,6 @@
 from PIL import Image
+import vector_compare
+import os
 
 # verify_colour(image) consumes the image of captcha, then prints out the
 #   the possible colours we want from the captcha
@@ -13,4 +15,16 @@ def verify_colour(image):
     for j,k in sorted(values.items(),key=lambda x:x[1],reverse = True)[:10]:
         print j,k
 
+def vector_crack(image,colour1,colour2=256):
+    v = VectorCompare()
+    iconset = ['0','1','2','3','4','5','6','7','8','9','0','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+    imageset = []
 
+    for letter in iconset:
+        for img in os.listdir('./iconset/%s/'%(letter)):
+            temp = []
+            if img != "Thumbs.db" and img != ".DS_Store":
+                temp.append(buildvector(Image.open("./iconset/%s/%s"%(letter,img))))
+            imageset.append({letter:temp})
+
+            
