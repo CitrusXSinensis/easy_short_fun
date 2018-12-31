1ed4b411ab45a6c9cc62600af5c5af9dc2ba743f
WeiningLi <bill.weining.li@gmail.com>
2018-12-30 22:55:08 -0500

    [Captcha_Cracker] update documentation

--- image_manipulations/Captcha_Cracker/crack.py
+++ image_manipulations/Captcha_Cracker/crack.py
@@ -20,25 +20,28 @@ def vector_crack(image,colour1,colour2=256):
     iconset = ['0','1','2','3','4','5','6','7','8','9','0','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
     imageset = []
 
+    # create imageset by using vector_compare module used to compare with elements in captcha
     for letter in iconset:
         for img in os.listdir('./iconset/%s/'%(letter)):
             temp = []
             if img != "Thumbs.db" and img != ".DS_Store":
                 temp.append(buildvector(Image.open("./iconset/%s/%s"%(letter,img))))
             imageset.append({letter:temp})
+
     im = Image.open("captcha.gif")
+
     # create a new white image with the same size of captcha
     im2 = Image.new("P",im.size,255)
 
     im.convert("P")
     temp = {}
 
+    # make im2 to be balck-white image with only wanted element on it
     for x in range(im.size[1]):
         for y in range(im.size[0]):
             pix = im.getpixel((y,x))
             temp[pix] = pix
             if pix == colour1 or pix == colour2:
-                # manipulate im2 to be balck-white image with only wanted element on it
                 im2.putpixel((y,x),0)
 
     inletter = False
@@ -48,6 +51,7 @@ def vector_crack(image,colour1,colour2=256):
 
     letters = []
 
+    # get the location of each elements in captcha
     for y in range(im2.size[0]): # slice across
         for x in range(im2.size[1]): # slice down
             pix = im2.getpixel((y,x))
@@ -66,6 +70,7 @@ def vector_crack(image,colour1,colour2=256):
 
     inletter=False
 
+    # compare each cropped element with training data we have and prints out the best guess
     for letter in letters:
         im3 = im2.crop((letter[0],0,letter[1],im2.size[1]))
 
