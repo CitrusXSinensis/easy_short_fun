d6fca5b3b0d6a3e9a5f4eb959d09aa3daf0c52ea
WeiningLi <bill.weining.li@gmail.com>
2018-12-31 23:38:54 -0500

    [Steganography] add function to encode data in image

--- image_manipulations/steganography.py
+++ image_manipulations/steganography.py
@@ -12,4 +12,13 @@ def convertToBin(myInt):
     myBin = "0"*(8-(len(bin(myInt))-2))+bin(myInt).replace('0b','')
     return myBin
 
+def encodeImage(image,data):
+    evenImage = transferToEven(image) # get copy of image with lsb changed to 0
+    binData = ''.join(map(convertToBin,bytearry(data,'utf-8')))
+    if len(binData) > 4 * len(image.getdata()):
+        raise Exception("Error: Given " + len(binData) + " bits, but image can only encode " + 4 * len(image.getdata()) + " bits")
+    encodedPixels = [(r+int(binData[index*4+0]),g+int(binData[index*4+1]),b+int(binData[index*4+2]),t+int(binData[index*4+3])) if index*4 < len(binary) else (r,g,b,t) for index,(r,g,b,t) in enumerate(list(evenImage.getdata()))] # 将 binary 中的二进制字符串信息编码进像素里
+    encodedImage = Image.new(evenImage.mode, evenImage.size)  # create image to store encoded pixels
+    encodedImage.putdata(encodedPixels)
+    return encodedImage
 
