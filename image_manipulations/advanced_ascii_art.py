0d202cf9f6f36f7b89cb439b008fc94a3bf6fb96
WeiningLi <bill.weining.li@gmail.com>
2019-01-01 20:58:36 -0500

    [advanced_ascii_art] add class to convert image to ascii art

--- image_manipulations/advanced_ascii_art.py
+++ image_manipulations/advanced_ascii_art.py
@@ -29,3 +29,30 @@ class CharFrame:
 				ascii_frame += self.pixelToChar(image[i, j])
 			ascii_frame += blank
 		return ascii_frame
+
+# ImageToChar is used to convert an image to ascii-art image
+class ImageToChar(CharFrame):
+	result = None
+	def __init__(self, path, upperSize=-1, fill=False, wrap=False):
+		self.genCharImage(path, upperSize, fill, wrap)
+
+	def genCharImage(self, path, upperSize=-1, fill=False, wrap=False):
+		image = cv2.imread(path, cv2.IMREAD_GRAYSCALE)
+		if image == None: return
+		self.result = self.convert(image, upperSize, fill, wrap)
+
+	def showImage(self, stream = 2):
+		if self.result == None: return
+		if stream == 1 and os.isatty(sys.stdout.fileno()):
+                        self.streamOut = sys.stdout.write
+                        self.streamFlush = sys.stdout.flush
+		elif stream == 2 and os.isatty(sys.stderr.fileno()):
+                        self.streamOut = sys.stderr.write
+                        self.streamFlush = sys.stderr.flush
+		elif hasattr(stream, 'write'):
+                        self.streamOut = stream.write
+                        self.streamFlush = stream.flush
+		self.streamOut(self.result)
+		self.streamFlush()
+		self.streamOut('\n')
+
